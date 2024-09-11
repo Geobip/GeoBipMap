f180190f61bad097e43b683f64e4f30077e0e934
geobip <ouahidimedb@gmail.com>
2024-09-11 22:32:26 +0100

    Data file in js

---
+++ js/data.js
@@ -0,0 +1,412 @@
+const data = {
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Jemaa el-Fnaa",
+          "city": "Marrakech"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [-7.9891, 31.6258]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Koutoubia Mosque",
+          "city": "Marrakech"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [-7.9895, 31.6248]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Majorelle Garden",
+          "city": "Marrakech"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [-7.9977, 31.6342]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Saadian Tombs",
+          "city": "Marrakech"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [-7.9915, 31.6220]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "El Badi Palace",
+          "city": "Marrakech"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [-7.9876, 31.6202]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Bahia Palace",
+          "city": "Marrakech"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [-7.9846, 31.6216]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Menara Gardens",
+          "city": "Marrakech"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [-7.9912, 31.6308]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Palais des Congrès",
+          "city": "Marrakech"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [-8.0100, 31.6311]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Marrakech Museum",
+          "city": "Marrakech"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [-7.9828, 31.6295]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Agdal Gardens",
+          "city": "Marrakech"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [-7.9934, 31.6211]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Marrakech Medina",
+          "city": "Marrakech"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [-7.9894, 31.6254]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Ben Youssef Madrasa",
+          "city": "Marrakech"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [-7.9857, 31.6335]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "El Hezaz Mosque",
+          "city": "Marrakech"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [-7.9821, 31.6291]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Museum of Moroccan Arts",
+          "city": "Marrakech"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [-7.9822, 31.6297]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Cyber Park",
+          "city": "Marrakech"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [-7.9928, 31.6249]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Palais de la Bahia",
+          "city": "Marrakech"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [-7.9852, 31.6217]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Mouassine Mosque",
+          "city": "Marrakech"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [-7.9865, 31.6340]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Jardin de la Koutoubia",
+          "city": "Marrakech"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [-7.9894, 31.6249]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Tanneries of Marrakech",
+          "city": "Marrakech"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [-7.9923, 31.6316]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Medersa Ben Youssef",
+          "city": "Marrakech"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [-7.9860, 31.6334]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Dar Si Said Museum",
+          "city": "Marrakech"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [-7.9879, 31.6212]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Hassan II Mosque",
+          "city": "Casablanca"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [-7.6036, 33.5890]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Royal Palace",
+          "city": "Casablanca"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [-7.6187, 33.5856]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Old Medina",
+          "city": "Casablanca"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [-7.6115, 33.5902]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Corniche",
+          "city": "Casablanca"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [-7.6370, 33.5774]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Casablanca Cathedral",
+          "city": "Casablanca"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [-7.6170, 33.5916]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Habous Quarter",
+          "city": "Casablanca"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [-7.6167, 33.5833]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Moroccan Jewish Museum",
+          "city": "Casablanca"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [-7.6177, 33.5854]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Ricks Café",
+          "city": "Casablanca"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [-7.6100, 33.5942]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Mohammed V Square",
+          "city": "Casablanca"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [-7.6038, 33.5895]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Central Market",
+          "city": "Casablanca"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [-7.6103, 33.5913]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Villa des Arts",
+          "city": "Casablanca"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [-7.6180, 33.5954]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Parc de la Ligue Arabe",
+          "city": "Casablanca"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [-7.6221, 33.5944]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "La Sqala",
+          "city": "Casablanca"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [-7.6107, 33.5884]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Anfa Place Living Resort",
+          "city": "Casablanca"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [-7.6335, 33.5948]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Palais Royal",
+          "city": "Casablanca"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [-7.6215, 33.5861]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Port de Casablanca",
+          "city": "Casablanca"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [-7.6046, 33.5910]
+        }
+      },]
+}
+
